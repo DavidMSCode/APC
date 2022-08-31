251e01172ff11b243c15efe6331ce7b46682ed21
David Stanley <davidms4@illinois.edu>
2022-08-31 14:10:38 -0500

    add global boolean flags header file

---
+++ include/flags.h
@@ -0,0 +1,15 @@
+/**
+ * @file flags.h
+ * @author David Stanley (davidms4@illinois.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __FLAGS__
+#define __FLAGS__
+    inline bool g_MATRICES_LOADED {false};        //Tells APC whether the EGM2008 matrices have already been loaded
+#endif
