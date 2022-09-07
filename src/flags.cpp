46e4321738f35ce6eab691619e38155cc7448039
David Stanley <davidms4@illinois.edu>
2022-09-07 16:57:18 -0500

    remove c++17 inline variables

---
+++ src/flags.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file flags.cpp
+ * @author David Stanley (davidms4@illinois.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "flags.h"
+bool g_MATRICES_LOADED = false;        //Tells APC whether the EGM2008 matrices have already been loaded
+bool g_DEBUG_MESSAGES = true;
