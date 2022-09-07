6bfd02f2750cb4bbe4d171d1c8c6d56574663c2f
David Stanley <davidms4@illinois.edu>
2022-09-07 16:57:38 -0500

    remove c++17 reqs

--- include/flags.h
+++ include/flags.h
@@ -11,6 +11,6 @@
 
 #ifndef __FLAGS__
 #define __FLAGS__
-    inline bool g_MATRICES_LOADED {false};        //Tells APC whether the EGM2008 matrices have already been loaded
-    inline bool g_DEBUG_MESSAGES {true};
+    extern bool g_MATRICES_LOADED;        //Tells APC whether the EGM2008 matrices have already been loaded
+    extern bool g_DEBUG_MESSAGES;
 #endif
