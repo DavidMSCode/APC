cea9e966a3b315564ae51086fe54d7edfd2bdac6
DavidMSCode <DavidMS4@Illinois.edu>
2024-12-09 16:50:44 -0600

    finished eccentric study

---
+++ src/main_interpolate_eccentric_tradestudy.cpp
@@ -0,0 +1,271 @@
+/*
+ *  AUTHORS:          David Stanley (DavidMS4@Illinois.edu)
+ *  DATE WRITTEN:     Nov 2023
+ * @ Modified by: Your name
+ * @ Modified time: 2023-12-24 17:44:44
+ * DESCRIPTION:      Set up an Adaptive-Picard-Chebyshev bootstrap trade study
+ * REFERENCE:        Woollands, R., and Junkins, J., "Nonlinear Differential Equation Solvers
+ *                   via Adaptive Picard-Chebyshev Iteration: Applications in Astrodynamics", JGCD, 2016.
+ */
+
+#include <string>
+#include <iostream>
+
+#include <vector>
+#include <iostream>
+#include <utility>
+#include <unistd.h>
+#include <fstream>
+#include <APC.h>
+#include <adaptive_picard_chebyshev.h>
+#include <c_functions.h>
+#include <Orbit.h>
+#include <EGM2008.h>
+#include <Ephemeris.hpp>
+#include "matrix_loader.h"
+#include "flags.h"
+#include "TwoBody.h"
+#include "const.h"
+#include "EphemerisRotation.h"
+using namespace std;
+
+struct outputs
+{
+    double TotalFevals;
+    double Hmax;
+    double PrepFevals;
+    double ForPIFevals;
+    double AftPIFevals;
+    double BootstrapFevals;
+};
+
+#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBWIDTH 60
+
+void printProgress(double percentage) {
+    int val = (int) (percentage * 100);
+    int lpad = (int) (percentage * PBWIDTH);
+    int rpad = PBWIDTH - lpad;
+    printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+    fflush(stdout);
+}
+
+vector<double> linspace(double a, double b, int n)
+{
+    vector<double> array;
+    double step = (b - a) / (n - 1);
+    for (int i=0;i<n;i++)
+    {
+        array.push_back(a + (step * i));
+    }
+
+    return array;
+}
+
+outputs run_trade_study_bootstrap(double alt, double d, double e, bool hot_finish, bool DisableBootstrap)
+{
+    // satellite properties
+    double mass = 212;        // sat mass (kg)
+    double area = 10;         // sat wetted area (m^2)
+    double reflectance = 1.5; // sat refelction absorption ratio
+    double drag_C = 2.0;      // sat coefficient of drag
+    // Perturbation calc flags
+    bool compute_drag = false;       // atmostpheric drag toggle
+    bool compute_SRP = false;        // Solar radiation pressure toggle
+    bool compute_third_body = false; // Third body gravity toggle
+    bool compute_hamiltonian = true; // whether or not the hamiltonian should be computed for the output
+    // Ephemeris
+    string spk = "de440.bsp";
+    string lsk = "naif0012.tls";
+    string center = "EARTH";
+    string frame = "J2000";
+
+    double a = C_Req + alt;
+    double i = 0.0;
+    double raan = 0.0;
+    double aop = 0.0;
+    double ta = 0.0;
+    vector<vector<double>> states = elms2rv(a, e, i, raan, aop, ta, C_MU_EARTH);
+
+    vector<double> r0 = states[0]; // Initial Position (km)
+    vector<double> v0 = states[1];
+    double speed = sqrt(pow(v0[0], 2) + pow(v0[1], 2) + pow(v0[2], 2));
+    double followtime = d / speed;                     // Follow time (s)
+    double T = 2 * C_PI * sqrt(pow(a, 3) / C_MU_EARTH); // Orbital period (s)
+    double t0 = 0;                                     // initial time (s)
+    double tf = t0 + T;                                // final time (s)
+
+    // Orbit orb = SinglePropagate(r0, v0, time_vec,  area,  reflectance,  mass,  drag_C,  compute_drag,  compute_SRP,  compute_third_body);
+    Orbit orbit("EARTH", "EARTH_IAU", "J2000");
+    orbit.SetProperties(area, reflectance, mass, drag_C);
+    orbit.SetPosition0(r0);
+    orbit.SetVelocity0(v0);
+    orbit.SetIntegrationTime(t0, tf);
+    orbit.SetComputeHamiltonian();
+    orbit.SetMaxDegree(200);
+    orbit.SetTolerance(1.e-15);
+    BootstrapOrbit bootstrap(orbit, followtime);
+    if(DisableBootstrap)
+    {
+        bootstrap.DisableBootstrap();
+    }
+
+    bootstrap.SetBootstrapHotFinish(hot_finish);
+    bootstrap.BootstrapPropagate();
+
+    //get fevals as catergories
+    double partialFevalRatio = pow(bootstrap.lowDeg,2)/pow(bootstrap.deg,2);
+    double PrepFevals = bootstrap.Feval.Prepare[0]+bootstrap.Feval.Prepare[1]*partialFevalRatio;
+    double ForPIFevals = bootstrap.forOrbit.Feval.PicardIteration[0]+bootstrap.forOrbit.Feval.PicardIteration[1]*partialFevalRatio;
+    double AftPIFevals = bootstrap.aftOrbit.Feval.PicardIteration[0]+bootstrap.aftOrbit.Feval.PicardIteration[1]*partialFevalRatio;
+    double BootstrapFevals = bootstrap.Feval.Bootstrap[0]+bootstrap.Feval.Bootstrap[1]*partialFevalRatio+ bootstrap.Feval.PicardIteration[0]+bootstrap.Feval.PicardIteration[1]*partialFevalRatio;
+
+    outputs output = {bootstrap.TotalFuncEvals, bootstrap.dHmax, PrepFevals, ForPIFevals, AftPIFevals, BootstrapFevals};
+    return output;
+}
+
+outputs run_trade_study_interpolate(double alt, double d, double e)
+{
+    // satellite properties
+    double mass = 212;        // sat mass (kg)
+    double area = 10;         // sat wetted area (m^2)
+    double reflectance = 1.5; // sat refelction absorption ratio
+    double drag_C = 2.0;      // sat coefficient of drag
+    // Perturbation calc flags
+    bool compute_drag = false;       // atmostpheric drag toggle
+    bool compute_SRP = false;        // Solar radiation pressure toggle
+    bool compute_third_body = false; // Third body gravity toggle
+    bool compute_hamiltonian = true; // whether or not the hamiltonian should be computed for the output
+    // Ephemeris
+    string spk = "de440.bsp";
+    string lsk = "naif0012.tls";
+    string center = "EARTH";
+    string frame = "J2000";
+
+    double a = C_Req + alt;
+    double i = 0.0;
+    double raan = 0.0;
+    double aop = 0.0;
+    double ta = 0.0;
+    vector<vector<double>> states = elms2rv(a, e, i, raan, aop, ta, C_MU_EARTH);
+
+    vector<double> r0 = states[0]; // Initial Position (km)
+    vector<double> v0 = states[1];
+    double speed = sqrt(pow(v0[0], 2) + pow(v0[1], 2) + pow(v0[2], 2));
+    double followtime = d / speed;                     // Follow time (s)
+    double T = 2 * C_PI * sqrt(pow(a, 3) / C_MU_EARTH); // Orbital period (s)
+    double t0 = 0;                                     // initial time (s)
+    double tf = t0 + T;                                // final time (s)
+
+    // Orbit orb = SinglePropagate(r0, v0, time_vec,  area,  reflectance,  mass,  drag_C,  compute_drag,  compute_SRP,  compute_third_body);
+    Orbit orbit("EARTH", "EARTH_IAU", "J2000");
+    orbit.SetProperties(area, reflectance, mass, drag_C);
+    orbit.SetPosition0(r0);
+    orbit.SetVelocity0(v0);
+    orbit.SetIntegrationTime(t0, tf);
+    orbit.SetComputeHamiltonian();
+
+    orbit.SetMaxDegree(200);
+
+    orbit.SetTolerance(1.e-15);
+    InterpolatedOrbit IntOrbit(orbit, followtime);
+    IntOrbit.InterpolatePropagate();
+
+    //get fevals as catergories
+    double partialFevalRatio = pow(IntOrbit.lowDeg,2)/pow(IntOrbit.deg,2);
+    double PrepFevals = IntOrbit.Feval.Prepare[0]+IntOrbit.Feval.Prepare[1]*partialFevalRatio;
+    double ForPIFevals = IntOrbit.forOrbit.Feval.PicardIteration[0]+IntOrbit.forOrbit.Feval.PicardIteration[1]*partialFevalRatio;
+    double AftPIFevals = 0;
+    double BootstrapFevals = IntOrbit.Feval.Bootstrap[0]+IntOrbit.Feval.Bootstrap[1]*partialFevalRatio+ IntOrbit.Feval.PicardIteration[0]+IntOrbit.Feval.PicardIteration[1]*partialFevalRatio;
+
+    outputs output = {IntOrbit.TotalFuncEvals, IntOrbit.dHmax, PrepFevals, ForPIFevals, AftPIFevals, BootstrapFevals};
+    return output;
+}
+
+
+int main(int argc, char **argv)
+{
+    // PRINT THE CURRENT WORKING DIRECTORY
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf("Current working dir: %s\n", cwd);
+    }
+    else
+    {
+        perror("getcwd() error");
+        return 1;
+    }
+
+    double alt = 120;
+    vector<double> es = {0.0,0.2,0.4,0.6,0.8};
+    // vector<double> alts = {960};
+    vector<double> spacings = linspace(0, 10, 101);
+    // vector<double> spacings = {0.26};
+    map<pair<double, double>, outputs> DataBootstrap;
+    map<pair<double, double>, outputs> DataInterpolate;
+    map<pair<double, double>, outputs> DataFinish;
+    map<pair<double, double>, outputs> DataNoBootstrap;   
+    int counter = 0;
+    // run_trade_study_bootstrap(120, 0, 0.2, false, true);
+    run_trade_study_interpolate(480, 0, 0.2);
+    // printProgress(0.0);
+    // for (double e : es)
+    // {
+    //     for (double d : spacings)
+    //     {
+    //         // cout << "Running alt: " << alt << " spacing: " << d << endl;
+    //         outputs output = run_trade_study_bootstrap(alt, d, e, false, false);
+    //         DataBootstrap.insert(make_pair(make_pair(e, d), output));
+    //         output = run_trade_study_interpolate(alt, d, e);
+    //         DataInterpolate.insert(make_pair(make_pair(e, d), output));
+    //         output = run_trade_study_bootstrap(alt, d, e, true, false);
+    //         DataFinish.insert(make_pair(make_pair(e, d), output));
+    //         output = run_trade_study_bootstrap(alt, d, e, false, true);
+    //         DataNoBootstrap.insert(make_pair(make_pair(e, d), output));
+    //         counter++;
+    //         printProgress((double)counter / (es.size() * spacings.size()));
+    //     }
+    // }
+    // // write Data to csv
+    // string headers = "Eccentricity,Spacing,Fevals,Hmax,Prepare Segmentation,Forward Satellite,Aftward Satellite,Bootstrapped Satellite";
+    // string filename = "INT_bootstrap_tradestudy_Earth.csv";
+    // ofstream myfile;
+    // myfile.open(filename);
+    // myfile << headers<<"\n";
+    // myfile << std::setprecision(15);
+    // for (auto const &x : DataBootstrap)
+    // {
+    //     myfile << x.first.first << "," << x.first.second << "," << x.second.TotalFevals << "," << x.second.Hmax << "," << x.second.PrepFevals << "," << x.second.ForPIFevals << "," << x.second.AftPIFevals << ","  << x.second.BootstrapFevals << "\n";
+    // }
+    // myfile.close();
+    // filename = "INT_bootstrap_finish_tradestudy_Earth.csv";
+    // myfile.open(filename);
+    // myfile << headers<<"\n";
+    // myfile << std::setprecision(15);
+    // for (auto const &x : DataFinish)
+    // {
+    //     myfile << x.first.first << "," << x.first.second << "," << x.second.TotalFevals << "," << x.second.Hmax << "," << x.second.PrepFevals << "," << x.second.ForPIFevals << "," << x.second.AftPIFevals << "," << x.second.BootstrapFevals << "\n";
+    // }
+    // myfile.close();
+    // filename = "INT_bootstrap_interpolate_tradestudy_Earth.csv";
+    // myfile.open(filename);
+    // myfile << headers<<"\n";
+    // myfile << std::setprecision(15);
+    // for (auto const &x : DataInterpolate)
+    // {
+    //     myfile << x.first.first << "," << x.first.second << "," << x.second.TotalFevals << "," << x.second.Hmax << "," << x.second.PrepFevals << "," << x.second.ForPIFevals << "," << x.second.AftPIFevals << "," << x.second.BootstrapFevals << "\n";
+    // }
+    // myfile.close();
+    // filename = "INT_bootstrap_no_bootstrap_tradestudy_Earth.csv";
+    // myfile.open(filename);
+    // myfile << headers<<"\n";
+    // myfile << std::setprecision(15);
+    // for (auto const &x : DataNoBootstrap)
+    // {
+    //     myfile << x.first.first << "," << x.first.second << "," << x.second.TotalFevals << "," << x.second.Hmax << "," << x.second.PrepFevals << "," << x.second.ForPIFevals << "," << x.second.AftPIFevals << "," << x.second.BootstrapFevals << "\n";
+    // }
+    // myfile.close();
+
+    return 0;
+}
